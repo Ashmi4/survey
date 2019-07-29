790157d933b9c74cf347310a453140ddd83b748f
Ashmi Patel <200388612@student.georgianc.on.ca>
2019-07-29 14:27:48 -0400

    Setup Footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Navbar from "../partials/navbar";
+import Footer from "../partials/footer";
 
 const Index = () => (
     
-    <div>
-        <Navbar/>
- <h1>This is Home Page</h1>
-    </div>
-
+    <div class="container-fluid">
+      <Navbar/>
+      <img src="..." class="img-fluid" alt="Responsive image"/>
+      <div class="media">
+  <img class="align-self-start mr-3" src="..." alt="Generic placeholder image"/>
+  <div class="media-body">
+    <h5 class="mt-0">Top-aligned media</h5>
+    <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+    <p>Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+  </div>
+</div>
+<Footer/>
+  </div>
+ 
 );
 
 export default Index;
---
+++ src/partials/footer.js
@@ -0,0 +1,78 @@
+import React from "react"
+
+const Footer = () => (
+    
+    
+<footer class="page-footer font-small blue pt-4">
+
+  
+  <div class="container-fluid text-center text-md-left">
+
+   
+    <div class="row">
+
+     
+      <div class="col-md-6 mt-md-0 mt-3">
+
+        
+        <h5 class="text-uppercase">SurveyApp</h5>
+        <p>www.surveyapp.com</p>
+        <p>11A College Crs</p>
+        <p>Barrie,ON -L4M 2W4</p>
+        <p>11A College Crs</p>
+
+      </div>
+      
+
+      <hr class="clearfix w-100 d-md-none pb-3"></hr>
+
+     
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        
+        <h5 class="text-uppercase">About US</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Who We Are</a>
+          </li>
+          <li>
+            <a href="#!">How it Works</a>
+          </li>
+          <li>
+            <a href="#!">Our Community</a>
+          </li>
+          
+        </ul>
+
+      </div>
+     
+      <div class="col-md-3 mb-md-0 mb-3">
+
+      
+        <h5 class="text-uppercase">Need Help</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Ask Me!</a>
+          </li>
+          
+        </ul>
+
+      </div>
+      
+    </div>
+    
+
+  </div>
+ 
+  <div class="footer-copyright text-center py-3">© 2019 Copyright:
+    <a href="">SurveyApp.com</a>
+  </div>
+  </footer>
+
+
+
+);
+
+export default Footer;
