2b877b96e10b3c468c7cb7e5d9a42e3f7c7f0f9b
Ashmi Patel <200388612@student.georgianc.on.ca>
2019-07-29 18:20:13 -0400

    homepage layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,25 @@ const Index = () => (
     
     <div class="container-fluid">
       <Navbar/>
-      <img src="http://cdn.strategyonline.ca/wp/wp-content/uploads/2015/12/shutterstock_253684528.jpg?73961c" class="img-fluid" alt="Responsive image"/>
       <section class="my-5">
 
    
 <div class="row">
 
- 
-  <div class="col-lg-12 col-xl-12">
+  
+  <div class="col-lg-5 col-xl-4">
+
+    
+    <div class="view overlay rounded z-depth-1-half mb-lg-0 mb-4">
+      <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/49.jpg" alt="Sample image"/>
+      <a>
+        <div class="mask rgba-white-slight"></div>
+      </a>
+    </div>
+
+  </div>
+  
+  <div class="col-lg-7 col-xl-8">
 
     
     <h3 class="font-weight-bold mb-3"><strong>Survey App</strong></h3>
@@ -22,10 +33,22 @@ const Index = () => (
     <p class="dark-grey-text">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit
       quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus
       et aut officiis debitis cum soluta nobis est eligendi placeat facere aut rerum.</p>
+      <p class="dark-grey-text">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit
+      quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus
+      et aut officiis debitis cum soluta nobis est eligendi placeat facere aut rerum.</p>
+      <p class="dark-grey-text">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit
+      quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus
+      et aut officiis debitis cum soluta nobis est eligendi placeat facere aut rerum.</p>
+      <p class="dark-grey-text">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit
+      quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus
+      et aut officiis debitis cum soluta nobis est eligendi placeat facere aut rerum.</p>
+      <p class="dark-grey-text">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit
+      quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus
+      et aut officiis debitis cum soluta nobis est eligendi placeat facere aut rerum.</p>
     
   
   
-    <a class="btn btn-primary btn-md" href="\about">Read more</a>
+    <a class="btn btn-primary btn-md" href="\surveypage">For Survey</a>
 
   </div>
   
